60e8c76dd4e54320afa910ac59eec10d226134f0
Steven Volckaert <steven.volckaert@gmail.com>
2016-12-28 12:59:47 +0100

    Uncommenting utility methods IPictureDispToIcon(IPictureDisp) and IPictureDispToImage(IPictureDisp). Adapting XML documentation tags. Moving using directives into the namespace declaration (SA1200).

--- StevenVolckaert.InventorPowerTools/StevenVolckaert.InventorPowerTools/ImageConvert.cs
+++ StevenVolckaert.InventorPowerTools/StevenVolckaert.InventorPowerTools/ImageConvert.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Drawing;
-using System.Security;
-using System.Security.Permissions;
-using System.Windows.Forms;
-using Inventor;
-
-namespace StevenVolckaert.InventorPowerTools
+﻿namespace StevenVolckaert.InventorPowerTools
 {
+    using System.Drawing;
+    using System.Security;
+    using System.Security.Permissions;
+    using System.Windows.Forms;
+    using Inventor;
+
     /// <summary>
-    /// Converts various image formats to Inventor.IPictureDisp objects and vice-versa.
+    /// Converts various image formats to <see cref="IPictureDisp"/> instances and vice-versa.
     /// <para>This class cannot be inherited.</para>
     /// </summary>
     [PermissionSet(SecurityAction.InheritanceDemand)]
@@ -18,51 +17,51 @@ namespace StevenVolckaert.InventorPowerTools
         /// Initializes a new instance of the <see cref="ImageConvert"/> class.
         /// </summary>
         private ImageConvert()
-            : base(String.Empty)
+            : base(string.Empty)
         {
         }
 
         /// <summary>
-        /// Converts a System.Drawing.Icon instance to the corresponding OLE IPictureDisp object.
+        /// Converts a <see cref="Icon"/> instance to an equivalent <see cref="IPictureDisp"/> instance.
         /// </summary>
-        /// <param name="icon">The System.Drawing.Icon instance to convert.</param>
-        /// <returns>A Inventor.IPictureDisp representing the OLE IPictureDisp object.</returns>
+        /// <param name="icon">The <see cref="Icon"/> instance to convert.</param>
+        /// <returns>A <see cref="IPictureDisp"/> instance equivalent to <paramref name="icon"/>.</returns>
         public static IPictureDisp FromIconToIPictureDisp(Icon icon)
         {
             return FromImageToIPictureDisp(icon.ToBitmap());
         }
 
         /// <summary>
-        /// Converts a System.Drawing.Image instance to the corresponding OLE IPictureDisp object.
+        /// Converts a <see cref="Image"/> instance to an equivalent <see cref="IPictureDisp"/> instance.
         /// </summary>
-        /// <param name="image">The System.Drawing.Image instance to convert.</param>
-        /// <returns>A Inventor.IPictureDisp representing the OLE IPictureDisp object.</returns>
+        /// <param name="image">The <see cref="Image"/> instance to convert.</param>
+        /// <returns>A <see cref="IPictureDisp"/> instance equivalent to <paramref name="image"/>.</returns>
         public static IPictureDisp FromImageToIPictureDisp(Image image)
         {
             return (IPictureDisp)GetIPictureDispFromPicture(image);
         }
 
-        ///// <summary>
-        ///// Converts a OLE IPictureDisp instance to the corresponding System.Drawing.Icon object.
-        ///// </summary>
-        ///// <param name="picture">The OLE IPictureDisp instance.</param>
-        ///// <returns>The corresponding System.Drawing.Icon instance.</returns>
-        //public static Icon IPictureDispToIcon(IPictureDisp picture)
-        //{
-        //    using (var bitmap = new Bitmap(IPictureDispToImage(picture)))
-        //    {
-        //        return Icon.FromHandle(bitmap.GetHicon());
-        //    }
-        //}
+        /// <summary>
+        /// Converts a <see cref="IPictureDisp"/> instance to an equivalent <see cref="Icon"/>  instance.
+        /// </summary>
+        /// <param name="pictureDisp">The <see cref="IPictureDisp"/> instance to convert.</param>
+        /// <returns>A <see cref="Icon"/> instance equivalent to <paramref name="pictureDisp"/>.</returns>
+        public static Icon IPictureDispToIcon(IPictureDisp pictureDisp)
+        {
+            using (var bitmap = new Bitmap(IPictureDispToImage(pictureDisp)))
+            {
+                return Icon.FromHandle(bitmap.GetHicon());
+            }
+        }
 
-        ///// <summary>
-        ///// Converts a OLE IPictureDisp instance to the corresponding System.Drawing.Image object.
-        ///// </summary>
-        ///// <param name="picture">The OLE IPictureDisp instance.</param>
-        ///// <returns>The corresponding System.Drawing.Image instance.</returns>
-        //public static Image IPictureDispToImage(IPictureDisp picture)
-        //{
-        //    return GetPictureFromIPicture(picture);
-        //}
+        /// <summary>
+        /// Converts a <see cref="IPictureDisp"/> instance to an equivalent <see cref="Image"/> instance.
+        /// </summary>
+        /// <param name="pictureDisp">The <see cref="IPictureDisp"/> instance.</param>
+        /// <returns>A <see cref="Image"/> instance equivalent to <paramref name="pictureDisp"/>.</returns>
+        public static Image IPictureDispToImage(IPictureDisp pictureDisp)
+        {
+            return GetPictureFromIPicture(pictureDisp);
+        }
     }
 }
