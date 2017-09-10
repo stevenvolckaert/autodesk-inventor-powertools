6c14c68cf240bc33056b99c7ca3d50c5af41da46
Steven Volckaert <steven.volckaert@gmail.com>
2017-09-10 10:49:36 +0200

    Updating version to 1.2.1.

--- StevenVolckaert.InventorPowerTools/Properties/AssemblyInfo.cs
+++ StevenVolckaert.InventorPowerTools/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 [assembly: Guid(StevenVolckaert.InventorPowerTools.AssemblyInfo.Guid)]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
